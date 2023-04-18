44be4b508e36ac3be57076d4f1f022a846999c79
Yashwardhan Singh Chandel <ytheading10@gmail.com>
2023-04-18 17:28:42 +0530

    Active Storage Added in Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :friend_requests_sent, class_name: "Friendrequest", foreign_key: "sender_id"
-  has_many :friend_requests_receivers, through: :friend_requests_sent, source: :receiver
+  has_many :friend_requests_sent, class_name: "Friendrequest", foreign_key: "sender_id", dependent: :destroy
+  has_many :friend_requests_receivers, through: :friend_requests_sent, source: :receiver, dependent: :destroy
 
-  has_many :friend_requests_received, class_name: "Friendrequest", foreign_key: "receiver_id"
-  has_many :friend_requests_senders, through: :friend_requests_received, source: :sender
+  has_many :friend_requests_received, class_name: "Friendrequest", foreign_key: "receiver_id", dependent: :destroy
+  has_many :friend_requests_senders, through: :friend_requests_received, source: :sender, dependent: :destroy
 
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_one_attached :image
 end
 
 
